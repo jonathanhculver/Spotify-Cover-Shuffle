3e6a3dbe8dd5fef84a039ec4ad6e16969ba62485
Jon Culver <jonathanhculver@gmail.com>
2014-03-20 20:23:58 -0400

    changing port for heroku

--- app.js
+++ app.js
@@ -54,7 +54,4 @@ app.post('/art', function(req, res){
 
 });
 
-var server = app.listen(3000, function() {
-    console.log('Listening on port %d', server.address().port);
-});
-
+var server = app.listen(process.env.PORT || 5000);
